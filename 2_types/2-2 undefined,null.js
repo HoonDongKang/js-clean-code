98c4c6a453a2ea3a56827b615a0e40195a4762f8
Donghoon Kang <d159123@naver.com>
2024-06-19 23:02:13 +0900

    2-2 undfeind,null

---
+++ 2_types/2-2 undefined,null.js
@@ -0,0 +1,19 @@
+!null; // true
+!!null; // false
+
+null === false; // false
+
+!null === true; // true
+
+let varb;
+
+typeof varb; // "undefined"
+
+verb = null;
+
+typeof varb; // "object"
+
+undefined + 10; // NaN
+null + 10; // 10
+
+!undefined; // true
--- 2_types/summary.md
+++ 2_types/summary.md
@@ -2,3 +2,10 @@
 
 자바스크립트는 동적인 타입을 갖는다 -> 타입검사가 용이하지 않음
 `typeof` `instanceof` `Object.prototype.toString.call()`
+
+## null VS undefined
+
+값이 없거나, 정의가 되지 않은 상태
+
+`null` -> 0 -> typeof Object
+`undefined` -> NaN -> typeof undefined
