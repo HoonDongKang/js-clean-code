429b3e6fc5686c65cdfcfd0b2140cbdaa908503f
Donghoon Kang <d159123@naver.com>
2024-07-10 01:36:53 +0900

    8-2 DOM API

--- 8_abstraction/8-2 DOM API.js
+++ 8_abstraction/8-2 DOM API.js
@@ -1,30 +1,3 @@
-## Magic number
-
-의미없는 숫자를 사용하는 것보다, 명시적으로 의미를 담고 있는 상수로 관리한다.
-
-```javascript
-const DELAY_MS = 3 * 60 * 1000;
-
-setTimeout(() => {
-  scrollToTop();
-}, DELAY_MS);
-
-// numeric operator
-const PRICE = {
-  MIN: 1_000_000,
-  MAX: 10_000_000,
-  // MIN: 1000000,
-  // MAX: 10000000,
-}
-```
-
-## DOM API
-
-html, css 를 작성하여 코드의 양이 증가하게 된다면 CRUD에 대한 코드가 복잡해질 수 있다.
-
--> 추상화를 통해서 분리를 시켜준다.
-
-```javascript
 const createLoader = () => {
   const el = document.createElement('div');
   const el2 = document.createElement('div');
@@ -50,4 +23,3 @@ const loader = () => {
 
   return newEl;
 }
-```
