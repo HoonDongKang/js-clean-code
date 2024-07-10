0c5510333cbaf77a97c3d8fcedfa888a47b2c8f4
Donghoon Kang <d159123@naver.com>
2024-07-11 01:11:13 +0900

    10-3 innerHTML

---
+++ 10_browser & web API/10-3 innerHTML.js
@@ -0,0 +1,4 @@
+const beforeEndElement = document.querySelector("main").insertAdjacentElement("beforeend", div);
+const beforeBeginElement = document.querySelector("main").insertAdjacentElement("beforebegin", div);
+const afterEndElement = document.querySelector("main").insertAdjacentElement("afterend", div);
+const afterBeginElement = document.querySelector("main").insertAdjacentElement("afterbegin", div);
